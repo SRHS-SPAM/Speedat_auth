1e41aa954ca95aee2f204c063545b1a57dac377e
UNCROOS <any492266@gmail.com>
2024-06-13 08:45:01 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
