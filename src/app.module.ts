ba348712b9b8c349241e73f42be01618a52d4f1f
UNCROOS <any492266@gmail.com>
2024-06-14 17:58:21 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, EmailModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
